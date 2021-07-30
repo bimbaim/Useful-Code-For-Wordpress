8aa9fe18aef0b93bc98c4a338ed6dbfbac3b1b12
bimbaim <ibrahimquraisy@gmail.com>
2021-07-30 14:35:45 +0700

    Update validation-contact-form-7.php

--- validation-contact-form-7.php
+++ validation-contact-form-7.php
@@ -24,22 +24,81 @@ add_filter('wpcf7_validate_textarea*', 'custom_textarea_validation_filter', 10, 
 
 function custom_text_validation_filter($result, $tag) {  
 $type = $tag['type'];
+$name = $tag['name'];/**
+ * CF7 Validation
+**/
+function custom_text_validation_filter($result, $tag) {  
+$type = $tag['type'];
 $name = $tag['name'];
-if($name == 'your-subject') {
+if($name == 'nama-lengkap' || $name == 'pesan' || $name == 'nomor-telpon') {
 $value = $_POST[$name];
+	
 		$nourl_pattern = '(http|https|href)';
     	$latin = "/^[a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ\s\d.('\"£$%!&*()}{@#~><>|=_+^?\/\\;:,,.)]+$/";
+		$html_tag = "<('[^']*'|'[^']*'|[^''>])*>";
+        $php_script = '\$|\{|\(|\)|\}';
+	
 if(preg_match($nourl_pattern,$value)){
 
-    $result->invalidate( $tag, "Subject cannot contain website addresses." );
+    $result->invalidate( $tag, "Tidak boleh memasukkan link url." );
                 }
                 
 else if(!preg_match($latin,$value)){
 
-    $result->invalidate( $tag, "Subject cannot contain non-latin word." );
+   $result->invalidate( $tag, "Harus memakai karakter latin." );
                 }
+	
+else if(!preg_match($html_tag,$value)){
+	
+   $result->invalidate( $tag, "Tidak boleh memasukkan html tag." );
+	
+				}
+                
+else if(!preg_match($php_script,$value)){
+	
+   $result->invalidate( $tag, "Tidak boleh memasukkan script." );
+	
+				}                
+	
 }
+
 return $result;
 }
 add_filter('wpcf7_validate_text','custom_text_validation_filter', 10, 2);
 add_filter('wpcf7_validate_text*', 'custom_text_validation_filter', 10, 2);
+
+function custom_textarea_validation_filter($result, $tag) {  
+$type = $tag['type'];
+$name = $tag['name'];
+if($name == 'pesan') {
+$value = $_POST[$name];
+		$nourl_pattern = '(http|https|href)';
+    	$latin = "/^[a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ\s\d.('\"£$%!&*()}{@#~><>|=_+^?\/\\;:,,.)]+$/";
+        $html_tag = "<('[^']*'|'[^']*'|[^''>])*>";
+        $php_script = '\$|\{|\(|\)|\}';
+        
+if(preg_match($nourl_pattern,$value)){
+
+    $result->invalidate( $tag, "Message cannot contain website addresses." );
+                }
+                
+else if(!preg_match($latin,$value)){
+
+    $result->invalidate( $tag, "Message cannot contain non-latin word." );
+                }
+else if(!preg_match($html_tag,$value)){
+	
+   $result->invalidate( $tag, "Tidak boleh memasukkan html tag." );
+	
+				}
+else if(!preg_match($php_script,$value)){
+	
+   $result->invalidate( $tag, "Tidak boleh memasukkan script." );
+	
+				}                   
+                
+}
+return $result;
+}
+add_filter('wpcf7_validate_textarea','custom_textarea_validation_filter', 10, 2);
+add_filter('wpcf7_validate_textarea*', 'custom_textarea_validation_filter', 10, 2);
